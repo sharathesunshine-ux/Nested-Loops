9e03eaf480fec31e0d851f11f09478ed4160f597
sharathesunshine-ux <sharathesunshine@gmail.com>
2026-04-27 15:21:01 +0530

    initial commit

---
+++ Character Occurence.py
@@ -0,0 +1,9 @@
+string = input("Please enter your own word:")
+char = input("Please enter your own Character:")
+i = 0
+count = 0
+while i<len(string):
+    if string[i] == char:
+        count = count + 1
+    i = i + 1
+print("The Total Number of Times",char,"has occured =",count)
